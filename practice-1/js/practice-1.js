9a08a03f35eda23de7ff3c8e45b3b7b9bf593fbe
Myroslav Shendrik <citrus.user2204@gmail.com>
2025-01-03 10:57:26 +0200

    Update practice-1.js

--- practice-1/js/practice-1.js
+++ practice-1/js/practice-1.js
@@ -1,5 +1,19 @@
 console.log('Practice-1 JS');
-console.log('Practice-1 JS');
 //! Код виконаного завдання
+const age = 16;
+console.log(age);
+
+let myAge;
+console.log(myAge);
+
+myAge = 17;
+console.log(myAge);
+
+myAge = 18;
+console.log(myAge);
+
+const myName = "Myroslav";
+console.log(myName);
+
 
 
