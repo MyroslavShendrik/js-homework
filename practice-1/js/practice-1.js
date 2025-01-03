f55c350a2bfc60c5f3d1c491450a958ed87cb5ce
Myroslav Shendrik <citrus.user2204@gmail.com>
2025-01-03 11:46:22 +0200

    lesson work

--- practice-1/js/practice-1.js
+++ practice-1/js/practice-1.js
@@ -1,9 +1,14 @@
 console.log('Practice-1 JS');
 //! Код виконаного завдання
+//? Код виконаного завдання
+//* Код виконаного завдання
+//todo Код виконаного завдання
 const age = 16;
 console.log(age);
 
-let myAge;
+// let myAge;
+let myAge = null;
+console.log(typeof myAge);
 console.log(myAge);
 
 myAge = 17;
@@ -15,5 +20,17 @@ console.log(myAge);
 const myName = "Myroslav";
 console.log(myName);
 
-
-
+console.log('------------');
+console.log('10');
+console.log(10);
+console.log("Myroslav")
+console.log(2008)
+console.log('------------');
+const typeMyAge = typeof myAge;
+console.log("typeMyAge:", typeMyAge);
+const typeMyName = typeof myName;
+console.log("тип зміної myName:", typeMyName);
+const isMan = true;
+console.log("стать чоловіча?:", isMan);
+const typeIsMan = typeof isMan;
+console.log("чи я чоловік?:", typeIsMan);
