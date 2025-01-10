f0ebb266a8b752f0923f67facc123ca95b501d63
Myroslav Shendrik <citrus.user2204@gmail.com>
2025-01-10 13:26:52 +0200

    homework-2

--- practice-1/js/practice-1.js
+++ practice-1/js/practice-1.js
@@ -40,9 +40,33 @@ console.log("lesson-2");
 const myNumber = 10;
 console.log('myNumber:', myNumber);
 console.log("тип змінної myNumber:", typeof myNumber);
-const promptNumber = prompt("ведіть число");
-console.log('promtNumber:', promptNumber);
-console.log("тип змінної promptNumber:", typeof promptNumber);
-const newPromptNumber = Number(promptNumber);
-console.log('newPromptNumber:', newPromptNumber);
-console.log("тип змінної newPromptNumber:", typeof newPromptNumber);
+// const promptNumber = prompt("ведіть число");
+// console.log('promtNumber:', promptNumber);
+// console.log("тип змінної promptNumber:", typeof promptNumber);
+ const newPromptNumber = Number(promptNumber);
+// console.log('newPromptNumber:', newPromptNumber);
+// console.log("тип змінної newPromptNumber:", typeof newPromptNumber);
+
+// lesson3
+console.log("lesson-3");
+console.log(36);
+console.log(true);
+console.log(false);
+console.log(null);
+console.log(undefined);
+
+console.log('typeof "lesson-3":', typeof "lesson-3");
+console.log(typeof 36);
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof null);
+console.log(typeof undefined);
+
+const a = 5;
+const b = 7;
+let c;
+c = a + b;
+console.log('a + b =', c);
+console.log('a - b =', a - b ); 
+console.log(' b / a =', b / a );
+console.log(' b * a =', b * a );
