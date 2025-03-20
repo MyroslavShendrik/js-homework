80be49db19be059fc6e1c99419ecd969a8a97279
Myroslav Shendrik <citrus.user2204@gmail.com>
2025-03-20 19:33:30 +0200

    Update lesson-9.js

--- lesson-9/js/lesson-9.js
+++ lesson-9/js/lesson-9.js
@@ -5,22 +5,22 @@ function isLoginValid(login) {
 }
 
 function isLoginUnique(allLogins, login) {
-    return allLogins.includes(login);
+    return !allLogins.includes(login);
 }
 
 function addLogin(allLogins, login) {
-    if (isLoginValid(login)) {
+    if (!isLoginValid(login)) {
         return 'Логін повинен бути від 4 до 16 символів';
     }
     
-    if (isLoginUnique(allLogins, login)) {
+    if (!isLoginUnique(allLogins, login)) {
         return 'Такий логін вже є';
     }
     
     allLogins.push(login);
     return 'Логін успішно доданий';
 }
-console.log(addLogin(logins, "Max"));
+console.log(addLogin(logins, "Maxs"));
 console.log(addLogin(logins, "Mipkol")); 
 console.log(addLogin(logins, "Alg"));  
 console.log(addLogin(logins, "4343434343434343434343434343434343434"));  
