e8980a0f58b78ebdd21200835553066c621ec086
MyroslavShendrik <citrus.user2204@gmail.com>
2025-04-13 23:03:49 +1400

    lesson-14

--- lesson-11/js/lesson-11.js
+++ lesson-11/js/lesson-11.js
@@ -1,54 +1,3 @@
-//todo [2]
-console.log(
-  "%c [2] ",
-  "color: yellow; background-color: #2274A5",
-);
-
-
-//? ✴️ Створіть об'єкт "weather" з трьома властивостями:
-//? "temperature", "humidity", "windSpeed"  
-//? та додайте їм будь-яких логічних значень значень.
-//? ✴️ За допомогою додавання властивостей
-//? додайте до об'єкту метод,
-//? який, використовуючи методи взаємодії з користувачем, 
-//? отримує значення температури та повертає "true",
-//? якщо температура нижче 0 градусів Цельсія, 
-//? та "false", якщо температура вище або дорівнює 0 градусів Цельсія. 
-//? ✳️ Якщо метод повернув "true" вивести повідомлення 
-//? “Температура нижче 0 градусів Цельсія” і навпаки, 
-//? дублюючи ці повідомлення  в консоль.
-//! Код виконаного завдання
-const weather = {
-  temperature: -9,
-  humidity: "80%",
-  windSpeed: "2 м/с",
-};
-
-weather.isTemperatureBelowZero = function () {
-  const input = prompt("Введіть температуру в градусах Цельсія:")?.trim;
-  const temperature = parseFloat(input);
-
-  if (isNaN(temperature)) {
-    alert("Будь ласка, введіть числове значення.");
-    return;
-  }
-
-  if (temperature < 0) {
-    alert("Температура нижче 0 градусів Цельсія");
-    console.log("Температура нижче 0 градусів Цельсія");
-    return true;
-  } else {
-    alert("Температура 0 градусів Цельсія або вище");
-    console.log("Температура 0 градусів Цельсія або вище");
-    return false;
-  }
-};
-
-console.log("weather:", weather);
-console.log("1️⃣ Temperature:", weather.temperature);
-
-
-
 //? ❗️❗️❗️ Для виконання всіх завдань необхідно використати ключове слово this ❗️❗️❗️
 //todo [1]
 console.log(
@@ -77,56 +26,112 @@ console.log(
 //? ✳️ За допомогою виклика методу "deposit" об'єкта "bankAccount" додай кошти на рахунок.
 //? ✳️ За допомогою виклика методу "withdraw" об'єкта "bankAccount" зніми кошти з рахуноку.
 //! Код виконаного завдання
-let bankAccount = {
-  ownerName: "Miroslav Shendryk",
-  accountNumber: "123456789",
-  balance: 5000,
+// let bankAccount = {
+//   ownerName: "Miroslav Shendryk",
+//   accountNumber: "123456789",
+//   balance: 5000,
 
-  deposit() {
-    let input = prompt("Введіть суму для поповнення:")?.trim(); 
-    if (!input) {
-      alert("⛔️ Ви нічого не ввели!");
-      console.log("⛔️ Ви нічого не ввели!");
-      return;
-    }
-    let amount = parseFloat(input);
-    if (isNaN(amount) || amount <= 0) {
-      alert("⛔️ Некоректна сума!");
-      console.log("⛔️ Некоректна сума!");
-      return;
-    }
-    this.balance += amount;
-    alert(`✅ Кошти додано. Залишок на рахунку: ${this.balance} грн`);
-    console.log(`✅ Кошти додано. Залишок на рахунку: ${this.balance} грн`);
-  },
+//   deposit() {
+//     let input = prompt("Введіть суму для поповнення:")?.trim(); 
+//     if (!input) {
+//       alert("⛔️ Ви нічого не ввели!");
+//       console.log("⛔️ Ви нічого не ввели!");
+//       return;
+//     }
+//     let amount = parseFloat(input);
+//     if (isNaN(amount) || amount <= 0) {
+//       alert("⛔️ Некоректна сума!");
+//       console.log("⛔️ Некоректна сума!");
+//       return;
+//     }
+//     this.balance += amount;
+//     alert(`✅ Кошти додано. Залишок на рахунку: ${this.balance} грн`);
+//     console.log(`✅ Кошти додано. Залишок на рахунку: ${this.balance} грн`);
+//   },
 
-  withdraw() {
-    let input = prompt("Введіть суму для зняття:")?.trim();
-    if (!input) {
-      alert("⛔️ Ви нічого не ввели!");
-      console.log("⛔️ Ви нічого не ввели!");
-      return;
-    }
-    let amount = parseFloat(input);
-    if (isNaN(amount) || amount <= 0) {
-      alert("⛔️ Некоректна сума!");
-      console.log("⛔️ Некоректна сума!");
-      return;
-    }
-    if (this.balance >= amount) {
-      this.balance -= amount;
-      alert(`✅ Кошти знято. Залишок на рахунку: ${this.balance} грн`);
-      console.log(`✅ Кошти знято. Залишок на рахунку: ${this.balance} грн`);
-    } else {
-      alert("⛔️ Недостатньо коштів на вашому рахунку!");
-      console.log("⛔️ Недостатньо коштів на вашому рахунку!");
-    }
+//   withdraw() {
+//     let input = prompt("Введіть суму для зняття:")?.trim();
+//     if (!input) {
+//       alert("⛔️ Ви нічого не ввели!");
+//       console.log("⛔️ Ви нічого не ввели!");
+//       return;
+//     }
+//     let amount = parseFloat(input);
+//     if (isNaN(amount) || amount <= 0) {
+//       alert("⛔️ Некоректна сума!");
+//       console.log("⛔️ Некоректна сума!");
+//       return;
+//     }
+//     if (this.balance >= amount) {
+//       this.balance -= amount;
+//       alert(`✅ Кошти знято. Залишок на рахунку: ${this.balance} грн`);
+//       console.log(`✅ Кошти знято. Залишок на рахунку: ${this.balance} грн`);
+//     } else {
+//       alert("⛔️ Недостатньо коштів на вашому рахунку!");
+//       console.log("⛔️ Недостатньо коштів на вашому рахунку!");
+//     }
+//   }
+// };
+
+// bankAccount.deposit();
+// bankAccount.withdraw();
+// console.log("--------------------------------------------------");
+
+//todo [2]
+console.log(
+  "%c [2] ",
+  "color: yellow; background-color: #2274A5",
+);
+
+
+//? ✴️ Створіть об'єкт "weather" з трьома властивостями:
+//? "temperature", "humidity", "windSpeed"  
+//? та додайте їм будь-яких логічних значень значень.
+//? ✴️ За допомогою додавання властивостей
+//? додайте до об'єкту метод,
+//? який, використовуючи методи взаємодії з користувачем, 
+//? отримує значення температури та повертає "true",
+//? якщо температура нижче 0 градусів Цельсія, 
+//? та "false", якщо температура вище або дорівнює 0 градусів Цельсія. 
+//? ✳️ Якщо метод повернув "true" вивести повідомлення 
+//? “Температура нижче 0 градусів Цельсія” і навпаки, 
+//? дублюючи ці повідомлення  в консоль.
+//! Код виконаного завдання
+const weather = {
+  temperature: -9,
+  humidity: "80%",
+  windSpeed: "2 м/с",
+};
+
+weather.isTemperatureBelowZero = function () {
+  let input = prompt("Введіть температуру в градусах Цельсія:");
+  console.log(input)
+  input = Number(input)
+  console.log(input)
+  const newTemperature = parseFloat(input);
+  console.log(newTemperature)
+  if (isNaN(newTemperature)) {
+    alert("Будь ласка, введіть числове значення.");
+    return;
+  }
+
+  if (newTemperature < 0) {
+    alert("Температура нижче 0 градусів Цельсія");
+    console.log("Температура нижче 0 градусів Цельсія");
+    return true;
+  } else {
+    alert("Температура 0 градусів Цельсія або вище");
+    console.log("Температура 0 градусів Цельсія або вище");
+    return false;
   }
 };
 
-bankAccount.deposit();
-bankAccount.withdraw();
-console.log("--------------------------------------------------");
+console.log("weather:", weather);
+console.log("1️⃣ Temperature:", weather.temperature);
+console.log(weather.isTemperatureBelowZero())
+
+
+
 
 
 
