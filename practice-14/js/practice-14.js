6bce69d5d467d0cdc2b7b82b1773c0c7fb4d0530
MyroslavShendrik <citrus.user2204@gmail.com>
2025-04-19 23:01:29 +1400

    Update practice-14.js

--- practice-14/js/practice-14.js
+++ practice-14/js/practice-14.js
@@ -25,3 +25,47 @@ const accessType1 = book.isPublic ? "публічному" : "закритому
 const message1 = `Книга ${book.title} автора ${book.author} з рейтингом ${book.rating} знаходиться в ${accessType1} доступі.`;
 console.log("message1:", message1);
 console.log(". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .");
+
+//? ✳️ Якщо потрібно окремо отримати змінні
+//? з такими ж назвами як власивості об'єкта
+//? та їх значеннями, то можна просто створити їх:
+// const title = "The Last Kingdom";
+// const author = "Bernard Cornwell";
+// const genres = ["historical prose", "adventure"];
+// const isPublic = true;
+// const rating = 8.38;
+
+// console.log("title:", title);
+// console.log("author:", author);
+// console.log("genres:", genres);
+// console.log("isPublic:", isPublic);
+// console.log("rating:", rating);
+// console.log("`  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `");
+
+// const accessType2 = isPublic ? "публічному" : "закритому";
+// const message2 = `Книга ${title} автора ${author} з рейтингом ${rating} знаходиться в ${accessType2} доступі.`;
+// console.log("message2:", message2);
+// console.log(". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .");
+
+//? ✴️ Для отримання змінних з такими ж назвами
+//? як власивості об'єкта та їх значеннями,
+//? використаємо синтаксис ❗️деструктуризованого присвоювання❗️. 
+//? ✳️ Деструктуризація завжди знаходиться у лівій частині операції присвоєння. 
+//? Змінним всередині фігурних дужок присвоюються значення властивостей об'єкта. 
+//? Якщо ім'я змінної та ім'я властивості збігаються, відбувається присвоювання, 
+//? в іншому випадку, їй буде присвоєно undefined. 
+//? Порядок оголошення змінних у фігурних дужках - неважливий.
+const { title, author, genres, isPublic, rating, coverImage } = book;
+
+console.log("title:", title);
+console.log("author:", author);
+console.log("genres:", genres);
+console.log("isPublic:", isPublic);  
+console.log("rating:", rating);
+console.log("coverImage:", coverImage); //! undefined
+console.log("`  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `  `");
+
+const accessType2 = isPublic ? "публічному" : "закритому";
+const message2 = `Книга ${title} автора ${author} з рейтингом ${rating} знаходиться в ${accessType2} доступі.`;
+console.log("message2:", message2);
+console.log("----------------------------------------------------------------------------------------------------------------------");
