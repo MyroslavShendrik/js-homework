0ef714097030a13f0d3bd1f4bce4cc4987d58564
MyroslavShendrik <citrus.user2204@gmail.com>
2025-07-01 20:02:14 +0300

    todo2 lesson-20
    Просто жах

--- lesson-20/js/lesson-20.js
+++ lesson-20/js/lesson-20.js
@@ -41,5 +41,40 @@ console.log(
 <div id="boxes"></div>
 */
 //! Код виконаного завдання
+const input = document.querySelector('#controls input');
+const renderBtn = document.querySelector('[data-action="render"]');
+const destroyBtn = document.querySelector('[data-action="destroy"]');
+const boxesContainer = document.querySelector('#boxes');
 
+renderBtn.addEventListener('click', () => {
+  const amount = Number(input.value);
+  createBoxes(amount);
+});
+destroyBtn.addEventListener('click', destroyBoxes);
+function createBoxes(amount) {
+  destroyBoxes(); 
+
+  const boxes = [];
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomColor();
+    box.style.marginBottom = '5px';
+    boxes.push(box);
+    size += 10;
+  }
+  boxesContainer.append(...boxes);
+}
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
 console.log("--------------------------------------------------");
