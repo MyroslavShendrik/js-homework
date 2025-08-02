432df09b8bba99326cbcade95674e1c94d503442
MyroslavShendrik <citrus.user2204@gmail.com>
2025-08-02 12:48:52 +0300

    ls

--- lesson-22/js/lesson-22.js
+++ lesson-22/js/lesson-22.js
@@ -1,21 +1,40 @@
+
 //!завдання 1
+//? Створіть слайдер на сторінці, який показує зображення.
+//?  При переміщенні слайдера виконуйте деякі дії, наприклад, змінюйте розмір зображення.
+//?  Використайте debounce для того, щоб ці дії виконувалися не занадто часто при швидкому
+//?  переміщенні слайдера.
+
+//! код:
 const slider = document.querySelector('.slider__input');
-  const image = document.querySelector('.slider__image');
+const image = document.querySelector('.slider__image');
+//! var1
+const resizeImage = _.debounce((value) => {
+  image.style.transform = `scale(${value / 50})`;
+}, 500);
+//! var2
+// const resizeImage = () => {
+//     const scaleValue = slider.value;
+//     image.style.width = `${scaleValue}%`;
+// };
+// const debouncedResize = _.debounce(resizeImage, 300);
+//! картинка заповзає на повзунок 
+slider.addEventListener('input', (e) => {
+  resizeImage(e.target.value);
+});
+//!завдання 2
+//? Потрібно забезпечити плавне переміщення об'єкту при русі мишкою.
+//? Рішення: використовуйте метод debounce з бібліотеки lodash.
+//? Встановіть час затримки в мілісекундах, наприклад 100мс, і передайте функцію,
+//? яка буде виконуватися при переміщенні мишкою.
+//! код:
+const box = document.getElementById('box');
 
-  const resizeImage = _.debounce((value) => {
-    image.style.transform = `scale(${value / 50})`;
-  }, 200); 
+const moveBox = _.debounce((x, y) => {
+  box.style.transform = `translate(${x}px, ${y}px)`;
+}, 300);
 
-  slider.addEventListener('input', (e) => {
-    resizeImage(e.target.value);
-  });
-  //!завдання 2
-  const box = document.getElementById('box');
-
-  const moveBox = _.debounce((x, y) => {
-    box.style.transform = `translate(${x}px, ${y}px)`;
-  }, 100); 
-
-  window.addEventListener('mousemove', (e) => {
-    moveBox(e.clientX, e.clientY);
-  });
+window.addEventListener('mousemove', (e) => {
+  moveBox(e.clientX, e.clientY);
+});
+//! зробити окремий область для цього квадрату, де тільки там він може рухатися 
