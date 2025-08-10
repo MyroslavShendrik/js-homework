535f73c778fb79708f82343575e831526799662e
MyroslavShendrik <citrus.user2204@gmail.com>
2025-08-10 10:27:10 +0300

    lesson

--- lesson-23/js/lesson-23.js
+++ lesson-23/js/lesson-23.js
@@ -1,9 +1,10 @@
 const images = document.querySelectorAll('.lazyload');
+const rootBox = document.querySelector('.images')
 
 function loadImage(img) {
   img.src = img.dataset.src;
   img.addEventListener('load', () => {
-    img.classList.add('lazyloaded'); 
+    img.classList.add('lazyloaded');
     console.log(`Зображення "${img.alt}" завантажене`);
   });
 }
@@ -13,10 +14,16 @@ const observer = new IntersectionObserver((entries, obs) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       loadImage(entry.target);
-      obs.unobserve(entry.target); 
+      obs.unobserve(entry.target);
     }
   });
-}, { threshold: 0.1 });
+},
+  {
+    // root: null,
+    root: rootBox,
+    threshold: 0.5
+  }
+);
 
 images.forEach(img => observer.observe(img));
 
